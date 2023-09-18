c846ee5ccbe582b06cfc66789b69e24681ee59b0
Chimey Palyul <palyul.chimey@gmail.com>
2023-09-18 09:57:13 -0600

    fetch

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
 const init = () => {
+    const inputForm = document.querySelector('form');
+    inputForm.addEventListener('submit',(event)=>{
+        event.preventDefault();
+        //event.target.children[1].value
+        //the above line of code will have the same effect as the following line of code which is /ed out
+        const input = document.querySelector('input#searchByID');
+        console.log(input.value);
+
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then((response) => response.json())
+        .then((data) => {
+            const title = document.querySelector("section#movieDetails h4")
+            const summary = document.querySelector("section#movieDetails p")
+            console.log(data);
+
+            title.innerText = data.title
+            summary.innerText = data.summary
+        })
+    });
   
 }
 
